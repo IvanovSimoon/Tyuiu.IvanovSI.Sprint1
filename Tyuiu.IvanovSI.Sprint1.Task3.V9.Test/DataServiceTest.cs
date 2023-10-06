f88f0663a1f637ee3fdbf9235f187e78cf14fd52
Семён Иванов <lilepopendr1@mail.ru>
2023-10-06 22:19:34 +0500

    Task3

---
+++ Tyuiu.IvanovSI.Sprint1.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IvanovSI.Sprint1.Task3.V9.Lib;
+namespace Tyuiu.IvanovSI.Sprint1.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExprssion()
+        {
+            DataService ds = new DataService();
+            double min = 150;
+            double res = ds.ConvertMinutesToHours((int)min);
+            Assert.AreEqual(res,2);
+
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovSI.Sprint1.Task3.V9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovSI.Sprint1.Task3.V9.Lib;
+
+namespace Tyuiu.IvanovSI.Sprint1.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Иванов С. И. | РПСб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задания #3                                                               *");
+            Console.WriteLine("* Вариант #9                                                               *");
+            Console.WriteLine("* Выполнил: Иванов Семён Иванович | РПСб-23-1                              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("* и печатает результат на экране.                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            double min;
+            int m;
+            int h;
+            
+
+            Console.WriteLine("Введите значение min в минутах:");
+            min = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            m = (int)min % 60;
+            h = (int)ds.ConvertMinutesToHours((int)min);
+            Console.WriteLine(min + " минут это " + h + " часов " + m + " минут");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+    }
+
