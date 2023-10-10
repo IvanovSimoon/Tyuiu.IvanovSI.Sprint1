923107a41e99fdfc034408fdc8e1cac530eb4b53
Семён Иванов <lilepopendr1@mail.ru>
2023-10-11 00:35:14 +0500

    Task6

---
+++ Tyuiu.IvanovSI.Sprint1.Task6.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovSI.Sprint1.Task6.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovSI.Sprint1.Task6.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59617cf0-409f-478d-b104-ef6cdf59ea09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanovSI.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovSI.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.IvanovSI.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Иванов С. И. | РПСб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задания #6                                                               *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнил: Иванов Семён Иванович | РПСб-23-1                              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить,                *");
+            Console.WriteLine("* что строка является символьным представлением натурального числа         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+
+
+
+            Console.WriteLine("Введите строку s:");
+            string s = Console.ReadLine();
+            
+
+
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            bool res = ds.CheckNumber(s);
+
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
